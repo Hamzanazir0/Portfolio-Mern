32d3536de6a964566e799e160419e38a6c97c785
Hamzanazir0 <Hamzanazir0@gmail.com>
2021-06-15 23:50:21 +0500

    asd

---
+++ myjs.js
@@ -0,0 +1,18 @@
+// $(window).ready(function() {
+//     var sec = $(".title-holder");
+//     var offset = sec.offset();
+//     var top = offset.top;
+
+//     $(".title-holder").stick_in_parent();
+
+//     $(window).scroll(function () {
+//         if(window.pageYOffset >= top ){
+//             $(".title-holder").stick_in_parent();
+//             console.log("Top is: " + top);
+//             console.log("Yoffset is: " + window.pageYOffset);
+//         }
+//         else{
+//             sec.removeClass("sticky");
+//         }
+//     });
+// });
